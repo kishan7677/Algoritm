087a4fbe45c4c1a7cf48f2550ee8f4449af29823
Kishan <44kishan@gmail.com>
2024-12-10 10:56:07 +0530

    Add existing file

--- TyepsOfCasting.cpp
+++ TyepsOfCasting.cpp
@@ -1,3 +1,12 @@
-//there are two types of casting in cpp
-//1. implicit casting (when casting is done by the compiler it is mainly of )
-//2. explicit casting ()
+// there are two types of casting in cpp
+// 1. implicit casting (when casting is done by the compiler it is mainly of )
+// 2. explicit casting (when casting done for userdefined data types with minimum data loss or no data loss)
+// a) static casting -> casting is done at compile time
+// b) dynamic casting -> when we want to cast base class refrence to derived class
+// c) reinterprit casting -> used to reinterpret memory of a variable as a different data types
+// d)const_cast: Used to add or remove const qualifiers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+}
