9f8a8e5fb58a7b3e5248d423bc684d0ff8e782b9
Kishan <44kishan@gmail.com>
2024-12-16 09:45:04 +0530

    Add existing file

---
+++ algorithm.cpp/pallindrome.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool isPalindrome(string s)
+{
+
+    int len = s.length();
+    string ogstr = "";
+
+    for (int i = 0; i < len; i++)
+    {
+
+        if (isalpha(s[i]) || (s[i] >= '0' && s[i] <= '9'))
+        {
+            ogstr.push_back(tolower(s[i]));
+        }
+    }
+    len = ogstr.length();
+
+    for (int i = 0; i < len / 2; i++)
+    {
+
+        if (ogstr[i] != ogstr[len - i - 1])
+            return false;
+    }
+    return true;
+}
+int main(){
+    string str1 = "radar";
+    cout<<isPalindrome(str1)<<endl;
+}
---
+++ interview.cpp
@@ -0,0 +1,25 @@
+//silent  listen
+void fun(){
+   string  str1 ,str2;
+   char ch[26]={0};
+   if (str1.length() != str2.length(){
+      return false ;
+   }
+   for(int =0 ; i<str1.length();i++){
+    int n=str1[i]-'a';
+    ch[n]=str1[i];
+   }
+   for(int j=0;j<str2.length();j++){
+    int n =str2[i]-'a';
+      ch[n]=0;
+      
+   }
+  for(int i=0;i<26;i++){
+    if(ch[i]==0){
+        return true;
+    }else return false;
+  }
+
+
+    
+}
