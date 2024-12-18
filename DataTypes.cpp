6ae8b92bb0d125185bd004e0e0d7f657228b1901
Kishan <44kishan@gmail.com>
2024-12-18 08:40:17 +0530

    Add existing file

---
+++ DataTypes.cpp
@@ -0,0 +1,7 @@
+//there are three types of data types in cpp
+//1. primitive data types
+//int ,char ,short ,double ,void ,bool ,wide char
+//2. user defined data types
+//struct ,class ,union ,enum
+//3. reference data types (Derivied data types)
+//array ,pointer ,function ,reference ,const ,volatile ,typename ,auto ,decltype ,typ
